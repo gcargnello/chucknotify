d212ca256d22b0f17d68342e758f4cd37f8638c6
Giuliano Cargnello <giuliano.cargnello@gmail.com>
2017-01-15 15:32:32 +0100

    .

--- Chuck.py
+++ Chuck.py
@@ -1,66 +1,74 @@
 import json
 import requests
 from Modules import html_decode
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
 
 ##################################################
 # LEGGE UN WITZ CHUCK NORRIS E LO MANDA A SLACK
 ##################################################
+def witz_to_slack():
+
+    # PROCEDURA MAIN recupera un witz a caso
+    url = 'http://api.icndb.com/jokes/random'
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        pass
+        witz = 'Non ne ho una da raccontare...'
+    else:
+        full_json = r.text
+        full = json.loads(full_json)
+        witz = (full['value']['joke'])
+
+    # togli i cartteri escape
+    witz = html_decode(witz)
+    print ('W:',witz)
 
 
-# PROCEDURA MAIN recupera un witz a caso
-url = 'http://api.icndb.com/jokes/random'
+    # e adesso inviala a slack
+    url_wh = 'https://hooks.slack.com/services/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'
 
-r = requests.get(url)
-
-if r.status_code != 200:
-    pass
-    witz = 'Non ne ho una da raccontare...'
-else:
-    full_json = r.text
-    full = json.loads(full_json)
-    witz = (full['value']['joke'])
-
-# togli i cartteri escape
-witz = html_decode(witz)
-print ('W:',witz)
+    usr = "WitzBOT"
+    icon = ":hugging_face:"
+    msg = "La sai l'ultima?"
+    text_a = witz
+    text_b = ""
+    channel = "vacanze"
 
 
-# e adesso inviala a slack
-url_wh = 'https://hooks.slack.com/services/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'
-
-usr = "WitzBOT"
-icon = ":hugging_face:"
-msg = "La sai l'ultima?"
-text_a = witz
-text_b = ""
-channel = "vacanze"
-
-
-payload = {'user' : usr,
-           'icon_emoji': icon,
-           'text':      msg,
-           'channel':   channel,
-           'attachments': [
-                            {   "color": "#36a64f",
-                                'text': text_a,
-                                'pretext': text_b,
-                                'author_name': "Chuck Norris",
-                                'author_link': "http://www.icndb.com/"
-#                                'author_icon': "http://flickr.com/icons/bobby.jpg"
-                            }
-                          ]
-           }
+    payload = {'user' : usr,
+               'icon_emoji': icon,
+               'text':      msg,
+               'channel':   channel,
+               'attachments': [
+                                {   "color": "#36a64f",
+                                    'text': text_a,
+                                    'pretext': text_b,
+                                    'author_name': "Chuck Norris",
+                                    'author_link': "http://www.icndb.com/"
+    #                                'author_icon': "http://flickr.com/icons/bobby.jpg"
+                                }
+                              ]
+               }
 
 
-# esegue chiamata
-r = requests.put(url_wh,data = json.dumps(payload),
-                 headers={'Content-Type': 'application/json'}
-                )
+    # esegue chiamata
+    r = requests.put(url_wh,data = json.dumps(payload),
+                     headers={'Content-Type': 'application/json'}
+                    )
 
-# controlla esito
-if (r.status_code != 200):
-    print ('Errore di connessione')
-    print (r.text)
-else:   
-    print (r.text)
+    # controlla esito
+    if (r.status_code != 200):
+        print ('Errore di connessione')
+        print (r.text)
+    else:   
+        print (r.text)
 
+    return '<h1>Inviato messaggio</h1>'
