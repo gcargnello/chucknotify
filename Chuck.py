5e5e8005fea3b29b0e8659ea3ae7d7ed18412651
Giuliano Cargnello <giuliano.cargnello@gmail.com>
2017-01-15 17:00:13 +0100

    .

--- Chuck.py
+++ Chuck.py
@@ -5,8 +5,8 @@ from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
-#@app.route('/slash-command-url', methods=['POST'])
-@app.route('/')
+@app.route('/slash-command-url', methods=['POST'])
+#@app.route('/')
 
 # The parameters included in a slash command request (with example values):
 #   token=gIkuvaNzQIHg97ATvDxqgjtO
