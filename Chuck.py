db74b39d7c90d08137ce4cb0a61a005357339741
Giuliano Cargnello <giuliano.cargnello@gmail.com>
2017-01-15 15:03:44 +0100

    creato sorgente

---
+++ Chuck.py
@@ -0,0 +1,81 @@
+import json
+import requests
+
+##################################################
+# LEGGE UN WITZ CHUCK NORRIS E LO MANDA A SLACK
+##################################################
+
+# escape decode
+def html_decode(s):
+    """
+    Returns the ASCII decoded version of the given HTML string. This does
+    NOT remove normal HTML tags like <p>.
+    """
+    htmlCodes = (
+            ("'", '&#39;'),
+            ('"', '&quot;'),
+            ('>', '&gt;'),
+            ('<', '&lt;'),
+            ('&', '&amp;')
+        )
+    for code in htmlCodes:
+        s = s.replace(code[1], code[0])
+    return s
+
+# PROCEDURA MAIN recupera un witz a caso
+url = 'http://api.icndb.com/jokes/random'
+
+r = requests.get(url)
+
+if r.status_code != 200:
+    pass
+    witz = 'Non ne ho una da raccontare...'
+else:
+    full_json = r.text
+    full = json.loads(full_json)
+    witz = (full['value']['joke'])
+
+# togli i cartteri escape
+witz = html_decode(witz)
+print ('W:',witz)
+
+
+# e adesso inviala a slack
+url_wh = 'https://hooks.slack.com/services/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'
+
+usr = "WitzBOT"
+icon = ":hugging_face:"
+msg = "La sai l'ultima?"
+text_a = witz
+text_b = ""
+channel = "vacanze"
+
+
+payload = {'user' : usr,
+           'icon_emoji': icon,
+           'text':      msg,
+           'channel':   channel,
+           'attachments': [
+                            {   "color": "#36a64f",
+                                'text': text_a,
+                                'pretext': text_b,
+                                'author_name': "Chuck Norris",
+                                'author_link': "http://www.icndb.com/"
+#                                'author_icon': "http://flickr.com/icons/bobby.jpg"
+                            }
+                          ]
+           }
+
+
+# esegue chiamata
+r = requests.put(url_wh,data = json.dumps(payload),
+                 headers={'Content-Type': 'application/json'}
+                )
+
+# controlla esito
+if (r.status_code != 200):
+    print ('Errore di connessione')
+    print (r.text)
+else:   
+    print (r.text)
+
