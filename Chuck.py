9bded5babfd609bb688e2aa18f3c2a7e7808a8ed
Giuliano Cargnello <giuliano.cargnello@gmail.com>
2017-01-15 22:54:41 +0100

    pc

--- Chuck.py
+++ Chuck.py
@@ -49,7 +49,7 @@ def witz_to_slack():
 
     # e adesso inviala a slack via slash command
     usr = request.form.get('user_name',None)
-    hello = 'ciao ' + usr + ', la sai l\'ultima?'
+    hello = 'Salve ' + usr + ', la sai l\'ultima?'
     icon = ":hugging_face:"
     msg = "La sai l'ultima?"
     text_a = witz
